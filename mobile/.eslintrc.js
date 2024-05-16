0e03ca615a95bf5ff61df936805a97abddddd769
leonardo <leonardolfelix12@gmail.com>
2024-05-16 17:31:57 -0300

    fix: eslint no backend parando de dar erro em tudo

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  extends: ['expo', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+      },
+    ],
   },
 };

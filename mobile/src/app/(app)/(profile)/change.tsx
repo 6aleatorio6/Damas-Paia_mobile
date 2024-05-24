9365cda67d1c05b84e66acf1057d659b92de11cb
leonardo <leonardolfelix12@gmail.com>
2024-05-24 16:21:10 -0300

    M - feat(app/profile): criado pagina de editar perfil

---
+++ mobile/src/app/(app)/(profile)/change.tsx
@@ -0,0 +1,58 @@
+import { clientQuery } from '@/app/_layout';
+import ButtonForm from '@/components/ButtonForm';
+import InputText from '@/components/InputText';
+import { useApi } from '@/lib/axiosApi';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
+
+export default function ChangeProfile() {
+  const user = clientQuery.getQueryData(['profile']) as
+    | { nome: string; avatar: unknown }
+    | undefined;
+
+  const stateValues = useState({
+    nome: user?.nome || '',
+    senha: undefined,
+  });
+
+  const { isPending, isError, mutate } = useApi('mutate', (axios) => ({
+    async mutationFn() {
+      return axios.put('/user', {
+        nome: stateValues[0].nome || undefined,
+        senha: stateValues[0].senha || undefined,
+      });
+    },
+    onSuccess() {
+      clientQuery.refetchQueries({ queryKey: ['profile'] });
+      Alert.alert('Alterado!', 'dados alterado com sucesso');
+      router.back();
+    },
+  }));
+
+  return (
+    <View className="px-20 pt-20 ">
+      <Text className="text-2xl font-bold mb-5">EDITAR PERFIL</Text>
+      <View className="mb-5">
+        <InputText
+          field="nome"
+          placeholder="Novo nome"
+          valState={stateValues}
+          textName="text-black "
+          className="text-black "
+        />
+        <InputText
+          field="senha"
+          placeholder="Nova senha"
+          valState={stateValues}
+          secureTextEntry
+          className="text-black "
+          textName="text-black "
+        />
+      </View>
+      <ButtonForm onPress={mutate}>Salvar alterações</ButtonForm>
+      {isPending && <Text className="text-center">Carregando...</Text>}
+      {isError && <Text className="text-center">Erro ao salvar</Text>}
+    </View>
+  );
+}

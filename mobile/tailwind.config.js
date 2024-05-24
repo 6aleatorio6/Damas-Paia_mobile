9ff91b9f7992775a4d0376ea758369a3cb48a0a8
leonardo <leonardolfelix12@gmail.com>
2024-05-23 22:44:40 -0300

    M - feat: ativando ttypedRoutes

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/App.{js,jsx,ts,tsx}', './src/app/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/components/*.{js,jsx,ts,tsx}',
+    './src/**/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },

a1a5ee05925b832fa70d876b1fb4a6e81637d4c8
leonardo <leonardolfelix12@gmail.com>
2024-06-24 21:25:23 -0300

    M - refactor: excluindo código do app para refatora-lo

--- mobile/src/app/(app)/(profile)/(index)/myFrases.tsx
+++
@@ -1,9 +0,0 @@
-import { TextPaia, ViewPaia } from '@/components/themes';
-
-export default function myFrases() {
-  return (
-    <ViewPaia ColorName="pri" className="flex-1 bg-black">
-      <TextPaia ColorName="pri">paia</TextPaia>
-    </ViewPaia>
-  );
-}

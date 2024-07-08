d8892c6b744c2fd439ef360d91ef8b62ff9144fa
leonardo <leonardolfelix12@gmail.com>
2024-07-08 19:23:54 -0300

    feat: hook de validacao funcionando

--- src/libs/useValidador.ts
+++ src/libs/useValidador.ts
@@ -1,23 +1,35 @@
-import { useEffect, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useState } from 'react';
 
 const TIMEOUT_VALIDY = 500;
 
-export function useValidador(value: string, validacoes: OpcaoDeValidacaoDocampo) {
+export function useValidador(value: string, validacoes: ValidacoesDoCampo) {
   const [validStatus, setValidStatus] = useState<StatusValidy>('OFF');
+  const errorRef = useRef<string | null>(null);
 
   useEffect(() => {
+    if (!validacoes) return setValidStatus('OFF');
     setValidStatus('LOADING');
 
-    const timeoutId = setTimeout(() => {
-      setValidStatus('VALIDY');
+    const timeoutId = setTimeout(async () => {
+      for (const verify of validacoes) {
+        const error = await verify(value);
+        errorRef.current = error || null;
+
+        if (!error) return setValidStatus('VALIDY');
+
+        setValidStatus('ERROR');
+      }
     }, TIMEOUT_VALIDY);
 
     return () => clearTimeout(timeoutId);
   }, [value]);
+
+  return { status: validStatus, error: errorRef.current };
 }
 
-export type OpcaoDeValidacaoDocampo =
-  | [(value: string) => boolean | string | PromiseLike<boolean | string>, string][]
+export type ValidacoesDoCampo =
+  | ((v: string) => PromiseLike<string | null | false | undefined> | string | null | false | undefined)[]
   | null;
 
-type StatusValidy = 'VALIDY' | 'ERROR' | 'LOADING' | 'OFF';
+export type StatusValidy = 'VALIDY' | 'ERROR' | 'LOADING' | 'OFF';

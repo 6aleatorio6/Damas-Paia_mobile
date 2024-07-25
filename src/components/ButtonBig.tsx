e15db47a64001b3bf15cb102379b3428a06da75d
leonardo <leonardolfelix12@gmail.com>
2024-07-25 02:08:59 -0300

    feat(component): ButtonBig melhorado

--- src/components/ButtonBig.tsx
+++ src/components/ButtonBig.tsx
@@ -1,37 +1,35 @@
-import { Colors } from '@/constants/colors';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
-interface Props {
-  children: string;
-  fontSize?: number;
-  backgroundColor: Colors;
-  color: Colors;
-  height?: number;
-  onPress?: () => void;
-}
-export default function ButtonBig({ onPress, children, fontSize, backgroundColor, color, height }: Props) {
-  const { button, text } = useStyles(stylesPaia).styles;
+export default function ButtonBig(props: Omit<TouchableOpacityProps, 'style'> & { style?: IButtonStyle }) {
+  const { styles } = useStyles(stylesPaia);
+  const { style = {}, children, ...propsButton } = props;
 
   return (
-    <TouchableOpacity style={button(backgroundColor, height || 40)} onPress={onPress}>
-      <Text style={text(fontSize, color)}>{children}</Text>
+    <TouchableOpacity style={styles.button(style)} {...propsButton}>
+      <Text style={styles.text(style)}>{children}</Text>
     </TouchableOpacity>
   );
 }
 
-const stylesPaia = createStyleSheet((theme, info) => ({
-  button: (color: Colors, height: number) => ({
-    flex: 1,
+const stylesPaia = createStyleSheet(({ colors }) => ({
+  button: ({ backgroundColor = colors.primary, height = '100%' }: IButtonStyle) => ({
+    backgroundColor,
+    height,
     justifyContent: 'center',
-    backgroundColor: theme.colors[color],
-    minHeight: height,
     borderRadius: 10,
   }),
-  text: (size = 27, color: Colors) => ({
-    color: theme.colors[color],
+  text: ({ color = colors.textPri, fontSize = 40 }: IButtonStyle) => ({
+    color,
+    fontSize,
     textAlign: 'center',
-    fontSize: size,
     fontWeight: 'bold',
   }),
 }));
+
+export interface IButtonStyle {
+  backgroundColor?: string;
+  color?: string;
+  fontSize?: number;
+  height?: number | `${number}%`;
+}

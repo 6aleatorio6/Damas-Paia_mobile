7603680d78bd82649b237127a8dc5ce0ea1218a1
leonardo <leonardolfelix12@gmail.com>
2024-07-25 02:42:46 -0300

    feat: validacoes e tema de cores melhorados

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,13 +1,16 @@
 export const colorsLight = {
-  //
+  // Principais
   primary: '#4F41BC',
   secondary: '#534C8A',
-  danger: 'C2240F',
+  danger: '#E53935',
+  warning: '#FFB300',
+  success: '#43A047',
 
-  // text
+  // Texto
   textPri: '#D7D7D7',
+  textSec: '#A1A1A1',
 
-  // body
+  // Corpo
   body: '#2C2B33',
   bodySec: '#444157',
 } as const;
--- src/libs/form/validacoes.ts
+++ src/libs/form/validacoes.ts
@@ -13,25 +13,17 @@ import { ValidacoesDoCampo } from './useValidador';
  */
 export const validsPaia = {
   email: [
-    (t) => t.length < 1 && 'Insira um email',
     (t) => t.length > 64 && 'Máx. 64 caracteres',
     (t) => !/^[a-zA-Z0-9._%+-]+/.test(t) && 'Caractere inválido no início',
     (t) => !/@[a-zA-Z0-9.-]+\./.test(t) && 'Formato de email inválido',
     (t) => !/[a-zA-Z]{2,}$/.test(t) && 'Terminação inválida',
     // verifyEmail && 'Erro ao verificar o email',
   ],
-  apelido: [
-    (t) => t.length < 1 && 'Insira um apelido/nome',
+  username: [
     (t) => (t.length < 3 || t.length > 40) && 'Entre 3 e 40 caracteres',
-    (t) => !/^[a-zA-Z\s\dÀ-ú]{3,40}$/.test(t) && 'Apenas letras, números e espaços',
+    (t) => /\s/.test(t) && 'Não pode conter espaço',
+    (t) => !/^[a-zA-Z]/.test(t) && 'Deve começar com uma letra',
+    (t) => !/^[a-zA-Z0-9._-]*$/.test(t) && 'apenas  (.), (_) e (-) são permitido',
   ],
-  nome: [
-    (t) => t.length < 1 && 'Insira um nome',
-    (t) => (t.length < 3 || t.length > 40) && 'Entre 3 e 40 caracteres',
-    (t) => !/^[a-zA-Z\s\dÀ-ú]{3,40}$/.test(t) && 'Apenas letras e espaços',
-  ],
-  senha: [
-    (t) => t.length < 1 && 'Insira uma senha',
-    (t) => (t.length < 4 || t.length > 40) && 'Entre 4 e 40 caracteres',
-  ],
+  senha: [(t) => (t.length < 4 || t.length > 40) && 'Entre 4 e 40 caracteres'],
 } satisfies Record<string, ValidacoesDoCampo>;

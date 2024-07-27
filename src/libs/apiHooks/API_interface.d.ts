a01ee35832725e182faadcc8aea0ea9034ab2845
leonardo <leonardolfelix12@gmail.com>
2024-07-27 02:02:31 -0300

    feat(apiHooks): adicionar interface User e hook useGetUser

---
+++ src/libs/apiHooks/API_interface.d.ts
@@ -0,0 +1,5 @@
+interface User {
+  uuid: string;
+  username: string;
+  email: string;
+}
---
+++ src/libs/apiHooks/querys.ts
@@ -0,0 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import useApi from './useApi';
+
+export const useGetUser = () =>
+  useApi('query', (axios) => ({
+    queryKey: ['user'],
+    queryFn: async () => axios.get<User>('/user').then((res) => res.data),
+  }));

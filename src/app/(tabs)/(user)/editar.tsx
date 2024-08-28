85d7cc6f18f5ada107b43a97b6aed28b9e3801ab
leonardo <leonardolfelix12@gmail.com>
2024-08-27 21:18:57 -0300

    refactor(screen/user): atualizar página de edição para usar hooks refatorados e mover lógica de mutation para o componente
    - Atualizado o formulário de edição de usuário para utilizar os novos hooks `useForm` e `useApi`
    - Movida a lógica de mutation diretamente para o componente `editar`, simplificando a estrutura
    - Removidos hooks antigos de mutation e opções de formulário, centralizando a lógica no componente

--- src/app/(tabs)/(user)/editar.tsx
+++ src/app/(tabs)/(user)/editar.tsx
@@ -1,50 +1,89 @@
+import { queryClientPaia } from '@/app/_layout';
 import ButtonSubmit from '@/components/ButtonSubmit';
 import Input from '@/components/FormInput';
 import FormMolde from '@/components/FormMolde';
 import FormSubmit from '@/components/FormSubmit';
-import { editarFormOptions, useDeleteUser } from '@/libs/apiHooks/mutations';
+import { useAuth } from '@/libs/apiHooks/authToken';
 import { useGetUser } from '@/libs/apiHooks/querys';
+import useApi from '@/libs/apiHooks/useApi';
+import { useForm } from '@/libs/form/formHooks';
 import { validsPaia } from '@/libs/form/validacoes';
-import { ScrollView, View } from 'react-native';
+import { router } from 'expo-router';
+import { Alert, ScrollView, View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 export default function Indextabs() {
+  const { logout } = useAuth();
   const { styles } = useStyles(stylesPaia);
   const { data } = useGetUser();
-  const optionsDelete = useDeleteUser();
+
+  const formName = useForm({
+    username: [(t) => t === data?.username && 'altere seu nome', ...validsPaia.username],
+  });
+  const formSenha = useForm();
+
+  const mutationPut = useApi('mutate', (axios) => ({
+    async mutationFn(values) {
+      await axios.put('/user', values);
+      await queryClientPaia.invalidateQueries({ queryKey: ['user'] });
+    },
+    onSuccess() {
+      Alert.alert('Sucesso', 'Usuário editado com sucesso');
+      router.navigate('/(tabs)/(user)');
+    },
+  }));
+
+  const mutationDelete = useApi('mutate', (axios) => ({
+    mutationFn: async () => {
+      Alert.alert('Tem certeza que deseja excluir sua conta?', 'Essa ação é irreversível', [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Excluir',
+          onPress: async () => {
+            await axios.delete('/user');
+            await logout();
+            await queryClientPaia.invalidateQueries();
+          },
+        },
+      ]);
+    },
+  }));
 
   return (
     <ScrollView>
       <View style={styles.container}>
-        <FormMolde
-          title="ALTERAR NOME"
-          style={styles.form}
-          titleStyle={styles.textStyle}
-          replaceValids={{
-            username: [(t) => t === data?.username && 'altere seu nome', ...validsPaia.username],
-          }}
-          submitOptions={editarFormOptions}
-        >
+        <FormMolde title="ALTERAR NOME" style={styles.form} titleStyle={styles.textStyle}>
           <Input
+            form={formName}
             field="username"
             name="Nome de usuario"
             textContentType="nickname"
             defaultValue={data?.username}
           />
-          <FormSubmit style={styles.buttonSubmit} height={70} title="SALVAR NOME" />
+          <FormSubmit
+            form={formName}
+            submit={mutationPut}
+            style={styles.buttonSubmit}
+            height={70}
+            title="SALVAR NOME"
+          />
         </FormMolde>
 
-        <FormMolde
-          title="ALTERAR SENHA"
-          style={styles.form}
-          titleStyle={styles.textStyle}
-          submitOptions={editarFormOptions}
-        >
-          <Input field="password" name="senha" textContentType="password" secureTextEntry />
-          <FormSubmit style={styles.buttonSubmit} height={70} title="SALVAR SENHA" />
+        <FormMolde title="ALTERAR SENHA" style={styles.form} titleStyle={styles.textStyle}>
+          <Input form={formSenha} field="password" name="senha" textContentType="password" secureTextEntry />
+          <FormSubmit
+            form={formSenha}
+            submit={mutationPut}
+            style={styles.buttonSubmit}
+            height={70}
+            title="SALVAR SENHA"
+          />
         </FormMolde>
 
-        <ButtonSubmit style={styles.buttonDelete} title="EXCLUIR CONTA" useApi={optionsDelete} />
+        <ButtonSubmit style={styles.buttonDelete} title="EXCLUIR CONTA" mutation={mutationDelete} />
       </View>
     </ScrollView>
   );

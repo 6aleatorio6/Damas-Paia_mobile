c9078dfc5945b30cbc7a43d21402a2be91ba5334
leonardo <leonardolfelix12@gmail.com>
2024-09-04 01:52:31 -0300

    test(setup): melhorando setup de testes
    - Adicionada configuração de temporizadores falsos e limpeza de mocks.
    - Configurado `queryClientPaia` para desativar retries.
    - Melhorado gerenciamento do MSW com limpeza do banco simulado e ajustes no ciclo de vida dos testes.

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -3,7 +3,8 @@ import '@testing-library/react-native/extend-expect';
 import 'fast-text-encoding';
 import 'react-native-url-polyfill/auto';
 import { setupServer } from 'msw/node'; // msw/native não funciona no @testing-library/react-native
-import { handlers } from './mocks.handlers';
+import { dbMock, handlers } from './mocks.handlers';
+import { queryClientPaia } from '@/libs/apiHooks/context/queryContext';
 
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
@@ -13,6 +14,19 @@ jest.mock('react-native-reanimated', () => null, { virtual: true });
 jest.mock('@testing-library/jest-native/extend-expect', () => null, { virtual: true });
 
 export const server = setupServer(...handlers);
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+
+beforeAll(() => {
+  jest.useFakeTimers();
+  server.listen();
+  queryClientPaia.setDefaultOptions({ queries: { retry: false } });
+});
+afterEach(() => {
+  dbMock.clear();
+  server.resetHandlers();
+  jest.clearAllMocks();
+});
+afterAll(() => {
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
+  server.close();
+});

4b952f35aaf8032bad8bcdc6534d2a97d87ebfdb
leonardo <leonardolfelix12@gmail.com>
2024-09-04 00:59:02 -0300

    test(utils): adicionar função para simular AsyncStorage em testes
    - Adicionada a função `AsyncStorageMockSimulator` para simular o AsyncStorage, facilitando testes de autenticação.
    - A função permite criar usuários simulados e configurar o AsyncStorage para cenários logados e deslogados:
      - `onlyDb`: adiciona um usuário ao banco de dados simulado sem salvar o token no AsyncStorage (usuário deslogado, mas cadastrado no db).
      - `dbAndStorage`: adiciona um usuário ao banco de dados simulado e salva o token no AsyncStorage (usuário logado).

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,5 +1,7 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { RouteSegments, StaticRoutes } from 'expo-router';
 import { renderRouter, screen, waitFor } from 'expo-router/testing-library';
+import { dbMock } from './mocks.handlers';
 
 export function renderRouterPaia(
   segments: RouteSegments<StaticRoutes>,
@@ -10,4 +12,28 @@ export function renderRouterPaia(
   return waitFor(() => {
     expect(screen).toHaveSegments(segments);
   });
+}
+
+/**
+ * Não simula perfeitamente o AsyncStorage, mas é suficiente para testes atualmente
+ *
+ * O ``addUser`` é opcional e pode ser:
+ *   - `onlyDb`: cria um usuário ao banco de dados mas não salva o token no AsyncStorage (deslogado, mas cadastrado)
+ *   - `dbAndStorage`: cria um usuário ao banco de dados e salva o token no AsyncStorage (logado)
+ */
+export function AsyncStorageMockSimulator(addUser?: 'onlyDb' | 'dbAndStorage') {
+  const map = new Map<string, string>() as any; // não preciso que seja exatamente igual ao AsyncStorage
+
+  const user = { username: 'paiaTriste', email: 'paia@gmail.com', password: '123456', uuid: '123456' };
+  if (addUser) {
+    dbMock.set(user.username, user);
+    if (addUser === 'dbAndStorage') map.set('token', user.username);
+  }
+
+  jest.spyOn(AsyncStorage, 'getItem').mockImplementation(map.get.bind(map));
+  jest.spyOn(AsyncStorage, 'setItem').mockImplementation(map.set.bind(map));
+  jest.spyOn(AsyncStorage, 'removeItem').mockImplementation(map.delete.bind(map));
+  jest.spyOn(AsyncStorage, 'clear').mockImplementation(map.clear.bind(map));
+
+  return user;
 }

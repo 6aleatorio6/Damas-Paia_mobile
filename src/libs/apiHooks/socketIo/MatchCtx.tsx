bca4b8baaf1815b48fe76f2a26bf0bfa0f5443c3
leonardo <leonardolfelix12@gmail.com>
2024-09-10 01:05:16 -0300

    feat(socketIo): adicionar contexto para gerenciamento do socket
    - Criar `MatchSocketProvider` para conectar e gerenciar o socket.
    - Implementar lógica para tratamento de erros de conexão e refresh de token.

---
+++ src/libs/apiHooks/socketIo/MatchCtx.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { io, Socket } from 'socket.io-client';
+import { createContext, PropsWithChildren, useContext, useEffect } from 'react';
+import { useAuth } from '../auth/tokenContext';
+import { baseURL, refreshTokenOrLogout } from '../auth/utils';
+
+type SocketPaia = Socket<ServerToCl, ClientToSv> & { data: any };
+const SocketContext = createContext<SocketPaia | null>(null);
+
+export function MatchSocketProvider(props: PropsWithChildren) {
+  const auth = useAuth();
+
+  const client = io(`${baseURL}/match`, {
+    autoConnect: false,
+    extraHeaders: { Authorization: `Bearer ${auth.token}` },
+  }) as SocketPaia;
+
+  useEffect(() => {
+    client.connect();
+
+    client.on('connect_error', async (e: any) => {
+      try {
+        // se disparar algum erro, quer dizer que não recebeu a mensagem que sinaliza que o token é inválido
+        const { message } = JSON.parse(e?.context?.responseText);
+
+        if (message === 'valid') throw e;
+        // Se o erro for de token expirado, tenta fazer o refresh do token
+        const tokenNovo = await refreshTokenOrLogout(auth);
+        client.io.opts.extraHeaders = { Authorization: `Bearer ${tokenNovo}` };
+        client.disconnect().connect();
+      } catch {
+        console.log('Erro ao conectar com o socket', e);
+      }
+    });
+
+    return () => {
+      client.disconnect();
+      client.off('connect_error');
+    };
+  }, []);
+
+  return <SocketContext.Provider value={client}>{props.children}</SocketContext.Provider>;
+}
+
+export function useMatchSocket() {
+  const socket = useContext(SocketContext);
+  if (!socket) throw new Error('useSocket usado fora do SocketProvider');
+  return socket;
+}

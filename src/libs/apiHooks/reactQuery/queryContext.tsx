03d55c0b4b65237b32aefb93ef985e700acbbcfc
leonardo <leonardolfelix12@gmail.com>
2024-09-11 00:32:05 -0300

    feat(apiHooks/reactQuery): configurar react-query para refazer healthcheck em erros de rede

--- src/libs/apiHooks/reactQuery/queryContext.tsx
+++ src/libs/apiHooks/reactQuery/queryContext.tsx
@@ -1,4 +1,5 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider, QueryObserverOptions } from '@tanstack/react-query';
+import axios from 'axios';
 import { PropsWithChildren } from 'react';
 
 const API_RETRIES = process.env['EXPO_PUBLIC_API_RETRIES'] || 3;
@@ -9,10 +10,27 @@ export const queryClientPaia = new QueryClient({
     queries: {
       retry: +API_RETRIES,
       staleTime: 1000 * 60 * +API_STALE_MINUTES,
+      throwOnError: OnErrorVerifyHealth,
+    },
+    mutations: {
+      throwOnError: OnErrorVerifyHealth,
     },
   },
 });
 
 export function QueryPaiaProvider(props: PropsWithChildren) {
   return <QueryClientProvider client={queryClientPaia}>{props.children}</QueryClientProvider>;
+}
+
+function OnErrorVerifyHealth(error: Error, query?: QueryObserverOptions) {
+  const ishealthcheck = query?.queryKey.includes('healthcheck');
+  const isErrorAxios = axios.isAxiosError(error);
+  if (ishealthcheck || !isErrorAxios) return false;
+
+  const isErrorConnection = error.status === 503 || error.status === 0 || !error.response;
+  if (isErrorConnection) {
+    queryClientPaia.refetchQueries({ queryKey: ['healthcheck'] }, { cancelRefetch: false });
+  }
+
+  return false;
 }

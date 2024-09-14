d844e99832848d0102e0eac20c89c9227169b8bd
leonardo <leonardolfelix12@gmail.com>
2024-09-13 22:59:26 -0300

    refactor(game/board): mover lógica de peças para componente separado e rotacionar tabuleiro para sempre o user ficar no lado de baixo

--- src/app/(tabs)/(game)/match.tsx
+++ src/app/(tabs)/(game)/match.tsx
@@ -1,25 +1,17 @@
 import Board from '@/components/game/Board';
-import { Piece } from '@/components/game/Piece';
+import Pieces from '@/components/game/Pieces';
 import PlayerHud from '@/components/game/PlayerHud';
-import { useMatchSocket } from '@/libs/apiHooks/socketIo/MatchCtx';
 import { useState } from 'react';
 import { View } from 'react-native';
 
 export default function Match() {
-  const socket = useMatchSocket();
-  const { myPlayer, playerOponent } = socket.data as MatchPaiado;
   const [squareSize, setSquareSize] = useState(0);
 
   return (
     <View style={{ width: '80%', alignSelf: 'center' }}>
       <PlayerHud />
-      <Board gridSize={8} useSquareSize={[squareSize, setSquareSize]}>
-        {playerOponent.pieces.map((piece) => (
-          <Piece key={piece.id} isQueen={piece.queen} x={piece.x} y={piece.y} gridSize={8} />
-        ))}
-        {myPlayer.pieces.map((piece) => (
-          <Piece key={piece.id} isQueen={piece.queen} x={piece.x} y={piece.y} isMyPiece gridSize={8} />
-        ))}
+      <Board useSquareSize={[squareSize, setSquareSize]}>
+        <Pieces squareSize={squareSize} />
       </Board>
       <PlayerHud isUser />
     </View>
--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -1,14 +1,18 @@
+import { useMatchSocket } from '@/libs/apiHooks/socketIo/MatchCtx';
 import { PropsWithChildren } from 'react';
 import { View } from 'react-native';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
+const gridSize = 8;
+
 interface Props extends PropsWithChildren {
-  gridSize: number;
   useSquareSize: [number, (size: number) => void];
 }
-export default function Board({ gridSize, children, useSquareSize }: Props) {
+export default function Board({ children, useSquareSize }: Props) {
   const { styles } = useStyles(stylesPaia);
   const [squareSize, setSquareSize] = useSquareSize;
+  const match = useMatchSocket().data as MatchPaiado;
+  const iAmPlayer1 = match.myPlayer.pieces[0].y === 0;
 
   const squares = [];
   for (let row = 0; row < gridSize; row++) {
@@ -19,7 +23,10 @@ export default function Board({ gridSize, children, useSquareSize }: Props) {
   }
 
   return (
-    <View onLayout={(e) => setSquareSize(e.nativeEvent.layout.width / gridSize)} style={styles.board}>
+    <View
+      onLayout={(e) => setSquareSize(e.nativeEvent.layout.width / gridSize)}
+      style={styles.board(iAmPlayer1)}
+    >
       {squares}
       {!!squareSize && children}
     </View>
@@ -27,12 +34,14 @@ export default function Board({ gridSize, children, useSquareSize }: Props) {
 }
 
 const stylesPaia = createStyleSheet((theme) => ({
-  board: {
+  board: (rotate180 = false) => ({
+    position: 'relative',
+    transform: rotate180 ? [{ rotate: '180deg' }] : undefined,
     width: '100%',
     aspectRatio: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
-  },
+  }),
   square: (isBlack: boolean, gridSize: number) => ({
     width: `${100 / gridSize}%`,
     height: `${100 / gridSize}%`,

30bbc6030b3ac8460d2df09f87df6e061d76fa3f
leonardo <leonardolfelix12@gmail.com>
2024-09-16 19:07:25 -0300

    feat(game/exit): adicionar modal de confirmação ao pressionar botão de voltar físico durante a partida

--- src/app/(tabs)/(game)/match.tsx
+++ src/app/(tabs)/(game)/match.tsx
@@ -1,11 +1,22 @@
 import Board from '@/components/game/Board';
 import Pieces from '@/components/game/Pieces';
 import PlayerHud from '@/components/game/PlayerHud';
-import { useState } from 'react';
-import { View } from 'react-native';
+import { useMatchSocket } from '@/libs/apiHooks/socketIo/MatchCtx';
+import { useEffect, useState } from 'react';
+import { BackHandler, View } from 'react-native';
 
 export default function Match() {
   const squareSizeState = useState(0);
+  const socket = useMatchSocket();
+
+  useEffect(() => {
+    const e = BackHandler.addEventListener('hardwareBackPress', () => {
+      socket.data.openModalExit(true);
+      return true;
+    });
+
+    return e.remove;
+  }, []);
 
   return (
     <View style={{ width: '90%', alignSelf: 'center' }}>

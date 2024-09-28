d3f409741ffd313d99751efe66123de22b48467e
leonardo <leonardolfelix12@gmail.com>
2024-09-27 22:40:15 -0300

    feat(oauth): criar componente com a lógica de login com OAuth2
    - Adicionado o componente `ButtonOauth` para gerenciamento de autenticação via OAuth2.
    - O componente implementa a lógica de obtenção do token e chamada à API para autenticação.
    - Inclui tratamento de erros e feedback visual.

---
+++ src/components/oauth2/ButtonOauth.tsx
@@ -0,0 +1,49 @@
+import { Alert, TouchableOpacity } from 'react-native';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+import { formatError } from '@/libs/apiHooks/auth/utils';
+import { useAxios } from '@/libs/apiHooks/auth/useAxios';
+import { useAuth } from '@/libs/apiHooks/auth/tokenContext';
+import { router } from 'expo-router';
+
+export interface ButtonOauthProps {
+  fetchOAuthToken: () => Promise<string>;
+  provider: string;
+  children?: React.ReactNode;
+}
+export default function ButtonOauth({ fetchOAuthToken, provider, children }: ButtonOauthProps) {
+  const { styles } = useStyles(stylesPaia);
+  const { setToken } = useAuth();
+  const axios = useAxios();
+
+  const handleOAuthLogin = async () => {
+    try {
+      const idToken = await fetchOAuthToken();
+      const { data } = await axios.get(`/oauth2/${provider}/${encodeURIComponent(idToken)}`);
+
+      await setToken(data.token);
+      router.replace('/(tabs)');
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro ao fazer login', formatError(error as Error));
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={handleOAuthLogin} style={styles.button}>
+      {children}
+    </TouchableOpacity>
+  );
+}
+
+const stylesPaia = createStyleSheet((theme) => ({
+  button: {
+    flexGrow: 1,
+    alignItems: 'center',
+    height: 50,
+    padding: 6,
+    backgroundColor: theme.colors.bodyTri,
+    borderColor: theme.colors.textSec,
+    borderRadius: 5,
+    borderWidth: 1,
+  },
+}));

5a7ce2b96e96e7ee63b1c93ae83a5b2146db7f65
leonardo <leonardolfelix12@gmail.com>
2024-10-01 00:50:08 -0300

    feat(components/avatar): adicionar animação de transição para o avatar
    - Implementada animação de transição ao carregar a imagem do avatar.
    - A imagem do avatar aparece suavemente ao ser carregada, ocultando o ícone de usuário padrão.
    - Utilizadas as propriedades `Animated` do React Native para controlar a opacidade durante a animação.
    - Modificado o estilo para garantir que a imagem do avatar se sobreponha ao ícone padrão.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,34 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { View, Animated } from 'react-native';
 import { UserCircle2 } from 'lucide-react-native';
-import { Image } from 'expo-image';
 import { createStyleSheet, useStyles } from 'react-native-unistyles';
 
 interface AvatarProps {
   url?: string | null;
   size: number;
 }
-
 export default function Avatar({ url, size }: AvatarProps) {
   const { styles, theme } = useStyles(stylesPaia);
+  const opacityShow = useRef(new Animated.Value(0)).current;
+  const opacityOcult = useRef(new Animated.Value(1)).current;
+  const [isFinalized, setIsFinalized] = useState(false);
+
+  const animaStart = () => {
+    if (isFinalized) return;
+
+    Animated.sequence([
+      Animated.timing(opacityOcult, {
+        toValue: 0,
+        duration: 250,
+        useNativeDriver: true,
+      }),
+      Animated.timing(opacityShow, {
+        toValue: 1,
+        duration: 250,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      setIsFinalized(true);
+    });
+  };
 
   return (
     <View style={[styles.container, { width: size, height: size }]}>
-      {url ? (
-        <Image source={{ uri: url }} style={styles.image(size)} />
-      ) : (
-        <UserCircle2 size={size} color={theme.colors.textPri} />
+      {url && (
+        <Animated.Image
+          onLoad={() => animaStart()}
+          source={{ uri: url }}
+          style={[styles.image(size), { opacity: opacityShow }]}
+        />
       )}
+      <Animated.View style={{ opacity: opacityOcult }}>
+        <UserCircle2 size={size} color={theme.colors.textPri} />
+      </Animated.View>
     </View>
   );
 }
 
-const stylesPaia = createStyleSheet(() => ({
+const stylesPaia = createStyleSheet(({ colors }) => ({
   container: {
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'relative',
   },
   image: (size: number) => ({
+    position: 'absolute',
     width: size,
     height: size,
     borderRadius: size / 2,
